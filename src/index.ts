a2ce3ae48f821877e72a54fbecb00e75beac72d7
Alek Evans <github@alekeagle.com>
2026-01-05 20:22:31 -0900

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,117 @@
+import { config } from 'dotenv';
+import { Client, Constants, Member } from 'oceanic.js';
+import OceanicInteractions from 'oceanic.js-interactions';
+
+import {
+  startCacheRefresh,
+  getSupporterByDiscordID,
+  emitter as PatreonEmitter,
+  cached as CachedPatreonMembers,
+} from './utils/Patreon.ts';
+
+config();
+
+// The server to manage roles for.
+const SERVER_ID = '785688843982471209',
+  // The role to assign to supporters.
+  ROLE_ID = '855933610778296330';
+
+const client = new Client({
+  auth: `Bot ${process.env.TOKEN}`,
+  gateway: {
+    intents: [Constants.Intents.GUILD_MEMBERS],
+    presence: {
+      status: 'online',
+      activities: [
+        {
+          name: 'Assigning Your Roles',
+          type: Constants.ActivityTypes.GAME,
+        },
+      ],
+    },
+    connectionProperties: {
+      browser: 'Discord Android',
+      device: 'Samsung Galaxy Z Flip5',
+      os: 'Android',
+    },
+  },
+});
+
+const handler = new OceanicInteractions.CommandHandler(client);
+
+// Leave a server if it is not the target server.
+client.on('guildCreate', async (guild) => {
+  if (guild.id !== SERVER_ID) {
+    console.log(`Leaving guild: ${guild.name} (${guild.id})`);
+    await guild.leave();
+  }
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}`);
+  // Start the Patreon cache refresh process.
+  startCacheRefresh();
+});
+
+client.on('guildMemberAdd', async (member) => {
+  if (member.guildID !== SERVER_ID) {
+    return;
+  }
+  console.log(`Member joined: ${member.user.username} (${member.id})`);
+  await checkAndAssignRoles(member);
+});
+
+PatreonEmitter.on('refreshed', async () => {
+  console.log('Patreon cache refreshed event received.');
+  // Iterate through all supporters and check if they are in the server.
+  for (const member of CachedPatreonMembers) {
+    if (!member.discord_id) {
+      console.log(`Supporter ${member.full_name} has no linked Discord ID.`);
+      continue;
+    }
+    const guildMember = await client.rest.guilds.getMember(
+      SERVER_ID,
+      member.discord_id,
+    );
+    if (guildMember) {
+      console.log(
+        `Checking roles for supporter ${guildMember.user.username} (${guildMember.id})`,
+      );
+      await checkAndAssignRoles(guildMember);
+    } else {
+      console.log(
+        `Supporter with Discord ID ${member.discord_id} is not in the server.`,
+      );
+    }
+  }
+});
+
+async function checkAndAssignRoles(member: Member): Promise<void> {
+  if (member.guildID !== SERVER_ID) {
+    console.log(`Member ${member.user.username} is not in the target server.`);
+    return;
+  }
+  const supporter = getSupporterByDiscordID(member.id);
+  if (supporter && supporter.lifetime_amount > 0) {
+    console.log(
+      `${member.user.username} has supported and will receive the role.`,
+    );
+    // Do they already have the role?
+    if (member.roles.includes(ROLE_ID)) {
+      console.log(`Role already assigned to ${member.user.username}`);
+      return;
+    }
+    await member.addRole(ROLE_ID, 'Patreon supporter joined the server');
+    console.log(`Assigned role to ${member.user.username}`);
+  } else {
+    console.log(
+      `${member.user.username} is not a supporter or has no lifetime support.`,
+    );
+  }
+}
+
+client.on('error', (error) => {
+  console.error('An error occurred:', error);
+});
+
+client.connect();
---
+++ src/utils/Patreon.ts
@@ -0,0 +1,163 @@
+import EventEmitter from 'node:events';
+
+const PATREON_API_URL = 'https://www.patreon.com/api/oauth2/v2';
+
+export type PatreonUser = {
+  amount: number;
+  lifetime_amount?: number;
+  status: 'active' | 'former' | 'declined' | 'free';
+  discord_id?: string;
+  full_name: string;
+  fetched_at: Date;
+  joined_at?: Date;
+};
+
+// Cache the Patreon API response for 30 minutes.
+export let cached: PatreonUser[] = [];
+
+export let emitter = new EventEmitter();
+
+let cacheTimeout: NodeJS.Timeout | null = null;
+
+export async function getAllMembers(): Promise<void> {
+  async function getMembersFromPage(
+    page: string = `${PATREON_API_URL}/campaigns/${process.env.PATREON_CAMPAIGN_ID}/members?include=currently_entitled_tiers,user&fields%5Bmember%5D=full_name,patron_status,currently_entitled_amount_cents,will_pay_amount_cents,lifetime_support_cents,pledge_relationship_start&fields%5Buser%5D=social_connections,full_name&sort=-pledge_relationship_start&page%5Bsize%5D=500`,
+  ) {
+    const fetchedAt = new Date(Date.now());
+    const response = await fetch(page, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${process.env.PATREON_ACCESS_TOKEN}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch members from Patreon API: ${response.statusText}`,
+      );
+    }
+
+    const json: any = await response.json(),
+      members = json.data,
+      users = json.included;
+
+    // Merge and cache the members and users.
+    members.forEach((member: any) => {
+      const user = users.find(
+        (i: any) => i.id === member.relationships.user.data.id,
+      );
+      if (user == null) {
+        return;
+      }
+      const patreonStatus =
+        member.attributes.patron_status === 'active_patron'
+          ? 'active'
+          : member.attributes.patron_status === 'former_patron'
+          ? 'former'
+          : member.attributes.patron_status === 'declined_patron'
+          ? 'declined'
+          : member.attributes.patron_status === null
+          ? 'free'
+          : 'unknown';
+      if (patreonStatus === 'unknown') {
+        throw new Error('Unknown Patreon status.');
+      }
+      cached.push({
+        amount: member.attributes.currently_entitled_amount_cents,
+        lifetime_amount:
+          member.attributes.campaign_lifetime_support_cents ??
+          member.attributes.lifetime_support_cents,
+        status: patreonStatus,
+        discord_id: user.attributes.social_connections.discord
+          ? user.attributes.social_connections.discord.user_id
+          : null,
+        full_name: user.attributes.full_name,
+        fetched_at: fetchedAt,
+        joined_at: new Date(member.attributes.pledge_relationship_start),
+      });
+    });
+
+    // Check if there are more pages to fetch.
+    if (json.meta.pagination?.cursors?.next) {
+      await getMembersFromPage(
+        `${PATREON_API_URL}/campaigns/${process.env.PATREON_CAMPAIGN_ID}/members?include=currently_entitled_tiers,user&fields%5Bmember%5D=full_name,patron_status,currently_entitled_amount_cents,will_pay_amount_cents,lifetime_support_cents,pledge_relationship_start&fields%5Buser%5D=social_connections,full_name&sort=-pledge_relationship_start&page%5Bsize%5D=500&cursor=${json.meta.pagination.cursors.next}`,
+      );
+    }
+  }
+  // Clear the cache before fetching.
+  cached = [];
+  // Fetch the members from the first page.
+  await getMembersFromPage();
+}
+
+export function getLatestSupporter(): PatreonUser | null {
+  // Check if the cache is empty.
+  if (cached.length === 0) {
+    return null;
+  }
+  // Get the newest supporter from the cached members.
+  const members = Object.values(cached).filter(
+    (member) => member.status === 'active',
+  );
+  members.sort((a, b) => {
+    return new Date(b.joined_at!).getTime() - new Date(a.joined_at!).getTime();
+  });
+  const member = members[0];
+
+  return member;
+}
+
+export function getSupporterByDiscordID(discordID: string): PatreonUser | null {
+  // Check if the cache is empty.
+  if (cached.length === 0) {
+    return null;
+  }
+  // Get the supporter from the cached members.
+  const member = cached.find((member) => member.discord_id === discordID);
+  if (member == null) {
+    return null;
+  }
+  return member;
+}
+
+export async function startCacheRefresh(): Promise<void> {
+  // Check if the cache is empty.
+  if (cached.length === 0) {
+    await getAllMembers();
+    // Emit an event that the cache has been refreshed.
+    emitter.emit('refreshed');
+  }
+  // Set a timeout to refresh the cache every 30 minutes.
+  if (cacheTimeout) {
+    clearTimeout(cacheTimeout);
+  }
+  cacheTimeout = setTimeout(async () => {
+    // Clear the cache before fetching.
+    cached = [];
+    // Set the timeout again.
+    startCacheRefresh().catch((error) => {
+      console.error('Failed to refresh Patreon cache:', error);
+    });
+  }, 30 * 60 * 1000);
+}
+
+export function stopCacheRefresh(): void {
+  // Clear the timeout if it exists.
+  if (cacheTimeout) {
+    clearTimeout(cacheTimeout);
+    cacheTimeout = null;
+  }
+}
+
+export async function forceCacheRefresh(): Promise<void> {
+  // Stop the current cache refresh.
+  stopCacheRefresh();
+  // Clear the cache before fetching.
+  cached = [];
+  // Restart the cache refresh, it will fetch fresh members immediately.
+  await startCacheRefresh();
+}
+
+export function getCachedMembers(): PatreonUser[] {
+  return cached;
+}
